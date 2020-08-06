cca2129661065a05808cb459fd7909d164c6acea
sealedtx <igorkiulian@gmail.com>
2020-08-06 17:21:12 +0300

    filter OTF formats; minor refactoring

--- src/main/java/com/github/kiulian/downloader/model/formats/Format.java
+++ src/main/java/com/github/kiulian/downloader/model/formats/Format.java
@@ -9,9 +9,9 @@ package com.github.kiulian.downloader.model.formats;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -45,7 +45,7 @@ public abstract class Format {
         Itag itag;
         try {
             itag = Itag.valueOf("i" + json.getInteger("itag"));
-        } catch (ExceptionInInitializerError e) {
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
             itag = Itag.unknown;
             itag.setId(json.getIntValue("itag"));
